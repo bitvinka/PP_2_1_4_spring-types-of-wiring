b350e67e1acb46307829a1b2e0ae08ab56b3af6b
Mariya Zem <alpakagold@gmail.com>
2023-12-12 01:40:32 +0300

    insert annotations, add .toString

--- src/main/java/koschei/Application.java
+++ src/main/java/koschei/Application.java
@@ -4,7 +4,8 @@ import koschei.config.AppConfig;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-public class Application {
+public class
+Application {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
